76bece1d66163c177a867c9f0aad65962ec6f873
Luka Jovanovic <luka@youshitsune.me>
2022-07-08 20:16:16 +0200

    Changed command for setting up wallpaper

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash 
 
 picom &
-if (( $(date '+%d') % 2 == 0))
-then
-	feh ~/.config/wallpapers/dark2.jpg --bg-scale
-else
-	feh ~/.config/wallpapers/dark1.jpg --bg-scale
-fi
+feh /home/luka/.config/wallpapers/01.jpg --bg-scale
 xinput set-prop "SynPS/2 Synaptics TouchPad" libinput\ Tapping\ Enabled 1
 dwmblocks &
 ibus-daemon -drxR
