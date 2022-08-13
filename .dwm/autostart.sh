1a0f7171fd111078017b0be32bf4bb20beea4979
Luka Jovanovic <luka@youshitsune.me>
2022-08-13 20:33:57 +0200

    Changed loading wallpaper and added Discord

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash 
 
 picom &
-feh /home/luka/.config/wallpapers/01.jpg --bg-scale
+feh /home/luka/.config/wallpapers/01.png --bg-scale
 xinput set-prop "SynPS/2 Synaptics TouchPad" libinput\ Tapping\ Enabled 1
 ibus-daemon -drxR
 dwmblocks &
 xset -dpms
 xset s off
+discord
