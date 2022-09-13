f966e399c3127c0e577cf3992597b3d24db88d67
Luka Jovanovic <luka@youshitsune.me>
2022-09-13 06:34:18 +0200

    Remove Logseq and move dwmblocks close to top

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -2,10 +2,9 @@
 
 picom &
 feh /home/luka/.config/wallpapers/01.png --bg-scale
+dwmblocks &
 xinput set-prop "SynPS/2 Synaptics TouchPad" libinput\ Tapping\ Enabled 1
 ibus-daemon -drxR
-dwmblocks &
 xset -dpms
 xset s off
 syncthing --no-browser &
-logseq
