ce1a457cc8af88ba096e028dcfc3b2fccc48eff6
Luka Jovanovic <luka@youshitsune.me>
2022-10-15 09:58:57 +0200

    Change wallpaper

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 picom &
-feh /home/luka/.config/wallpapers/01.png --bg-scale
+feh /home/luka/.config/wallpapers/02.png --bg-scale
 dwmblocks &
 xinput set-prop "SynPS/2 Synaptics TouchPad" libinput\ Tapping\ Enabled 1
 ibus-daemon -drxR
