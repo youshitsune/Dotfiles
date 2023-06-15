c2237d7ecb301fe1e3756bb549a80d14b10c9f70
youshitsune <youshitsune@dmz.rs>
2023-06-15 15:59:55 +0200

    update autostart script with new wallpaper extension

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 picom &
-feh /home/luka/.config/wallpapers/01.jpg --bg-scale
+feh /home/luka/.config/wallpapers/01.png --bg-scale
 slstatus &
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Tap Action" 1 1 1 2 1 3
 ibus-daemon -drxR
