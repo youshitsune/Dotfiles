1fb8c0c2a3c3c2591d78ac0a7b56bbab5703c583
youshitsune <youshitsune@dmz.rs>
2024-08-14 17:14:30 +0200

    move all config to this repo

---
+++ .config/nvim/lua/youshitsune/packer.lua
@@ -0,0 +1,38 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use({
+        'bluz71/vim-moonfly-colors',
+        as="moonfly",
+	})
+
+	use('nvim-treesitter/nvim-treesitter', {run = ":TSUpdate"})
+	use('nvim-treesitter/playground')
+	use('theprimeagen/harpoon')
+	use('mbbill/undotree')
+
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			--- Uncomment the two plugins below if you want to manage the language servers from neovim
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+
+			{'neovim/nvim-lspconfig'},
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	}
+
+    use('fatih/vim-go', { run = ":GoUpdateBinaries"})
+end)
